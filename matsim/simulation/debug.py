6392204e38f06e6291a71e7f7424e3dd8486ef12
Juste Raimbault <juste.raimbault@polytechnique.edu>
2021-01-15 16:56:54 +0000

    .

--- matsim/simulation/debug.py
+++ matsim/simulation/debug.py
@@ -3,6 +3,7 @@ import os.path
 
 def configure(context):
     context.stage("data.spatial.departments")
+    context.config("output_prefix")
 
 def execute(context):
     df_shape = context.stage("data.spatial.departments")[["departement_id", "geometry"]].rename(columns = dict(departement_id = "id"))
