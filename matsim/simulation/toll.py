acf5df295b6074b2df52850b4f731985deb07477
Juste Raimbault <juste.raimbault@polytechnique.edu>
2021-01-15 17:37:37 +0000

    run

--- matsim/simulation/toll.py
+++ matsim/simulation/toll.py
@@ -8,18 +8,39 @@ def configure(context):
     context.stage("matsim.runtime.java")
     context.stage("matsim.runtime.eqasim")
 
+    context.config("cordon_toll_price")
+    context.config("cordon_toll_name")
+    context.config("cordon_toll_shapefile")
+    context.config("data_path")
+    context.config("output_path")
+
 def execute(context):
 
     # add cordon toll around Paris
     tollprice = context.config("cordon_toll_price")
     if tollprice is None:
         tollprice = 0.0
+
+    print("Calling java RunCordonTollSetup from "+context.path())
+
     if context.config("cordon_toll_shapefile") is not None and context.config("cordon_toll_name") is not None:
         eqasim.run(context, "org.eqasim.core.scenario.spatial.RunCordonTollSetup", [
-            "--input-path", "%snetwork.xml.gz" % context.config("output_prefix"),
-            "--output-path", "%stoll_links.xml" % context.config("output_prefix"),
-            "--shape-path", context.config("data_prefix")+"/"+context.config("cordon_toll_shapefile"),
+            "--input-path", "%s/%snetwork.xml.gz" % (context.path("matsim.simulation.prepare"),context.config("output_prefix")),
+            "--output-path", "%stoll.xml" % context.config("output_prefix"),
+            "--shape-path", context.path()+"/../../"+context.config("data_path")+"/"+context.config("cordon_toll_shapefile"),
             "--shape-attribute", "name",
             "--shape-value", context.config("cordon_toll_name"),
             "--price", tollprice
         ])
+
+        shutil.copy(
+            "%s/%s" % (context.path(), "%stoll.xml" % context.config("output_prefix")),
+            "%s/%s" % (context.config("output_path"), "%stoll.xml" % context.config("output_prefix"))
+        )
+
+        # also copy toll file to matsim.simulation.prepare since the java sim module use its prefix - could have included this step in preparation, but avoids recomputing everything between two scenarii
+        shutil.copy(
+            "%s/%s" % (context.path(), "%stoll.xml" % context.config("output_prefix")),
+            "%s/%s" % (context.path("matsim.simulation.prepare"), "%stoll.xml" % context.config("output_prefix"))
+        )
+
