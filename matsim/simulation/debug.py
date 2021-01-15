dfde6b6eb89add631fffe95f485d69d7228b9dee
Juste Raimbault <juste.raimbault@polytechnique.edu>
2021-01-15 16:13:10 +0000

    fix

---
+++ matsim/simulation/debug.py
@@ -0,0 +1,10 @@
+import shutil
+import os.path
+
+def configure(context):
+    context.stage("data.spatial.departments")
+
+def execute(context):
+    df_shape = context.stage("data.spatial.departments")[["departement_id", "geometry"]].rename(columns = dict(departement_id = "id"))
+    df_shape["id"] = df_shape["id"].astype(str)
+    df_shape.to_file("%s/departments.shp" % context.path())
